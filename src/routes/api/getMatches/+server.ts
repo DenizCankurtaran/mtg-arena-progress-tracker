4ef747a8a10b356df0f160e1c9eb21e4c468c357
Deniz Cankurtaran <deniz.cankurtaran@hotmail.de>
2022-12-25 23:36:35 +0100

    writing to google sheets possible

--- src/routes/api/getMatches/+server.ts
+++ src/routes/api/getMatches/+server.ts
@@ -4,15 +4,9 @@ import { GOOGLE_APPLICATION_CREDENTIALS, SHEET_ID } from '$env/static/private';
  
 /** @type {import('./$types').RequestHandler} */
 export const GET: RequestHandler = async ({ request }) => {
-
-
 	process.env.GOOGLE_APPLICATION_CREDENTIALS = GOOGLE_APPLICATION_CREDENTIALS
-	//console.log(process.env);
-	//console.log(GOOGLE_APPLICATION_CREDENTIALS);
-	
-  	// return json({ status: 200, body: 'ok' })
-	const auth = await google.auth.getClient({scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly']});
 
+	const auth = await google.auth.getClient({scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly']});
 	const sheets = google.sheets({version: 'v4', auth});
 
 	const range = `Sheet1!A1:B1`;
@@ -22,9 +16,8 @@ export const GET: RequestHandler = async ({ request }) => {
 		range,
 
 	})
-	console.log(response);
+	console.log(response.data.values);
 	
 
-	//console.log(secret.private_key);
-  return json({ status: 200, body: 'ok' })
+  return json({ status: 200, body: response.data.values })
 }
--- src/routes/api/saveMatch/+server.ts
+++ src/routes/api/saveMatch/+server.ts
@@ -1,11 +1,29 @@
-//import { GOOGLE_APPLICATION_CREDENTIAL } from '$lib/server/secrets';
-//import secret from '$lib/server/secrets.json'
+import { GOOGLE_APPLICATION_CREDENTIALS, SHEET_ID } from '$env/static/private';
 import { json, type RequestHandler } from '@sveltejs/kit';
 import { google } from 'googleapis';
- 
+
 /** @type {import('./$types').RequestHandler} */
 export const POST: RequestHandler = async ({ request }) => {
-	const auth = await google.auth.getClient();
-	//console.log(secret.private_key);
-  return json({ status: 200, body: 'ok' })
+	process.env.GOOGLE_APPLICATION_CREDENTIALS = GOOGLE_APPLICATION_CREDENTIALS
+
+	const auth = await google.auth.getClient({ scopes: ['https://www.googleapis.com/auth/spreadsheets'] });
+	const sheets = google.sheets({ version: 'v4', auth });
+
+	const resource = {
+		id: 'test',
+		date: JSON.stringify(new Date())
+	}
+
+	const response = await sheets.spreadsheets.values.append({
+		spreadsheetId: SHEET_ID,
+		range: 'Sheet1!A:C',
+		valueInputOption: 'USER_ENTERED',
+		requestBody: {
+			majorDimension: 'ROWS',
+			values: [['test', 'hello']]
+		},
+	})
+	console.log(response);
+	
+	return json({ status: 200, body: 'ok' })
 }
