ea393c49a9e5d270dcdec579df44015af046a674
Görkem Yalcinkaya <goerkem.yalcinkaya@gmx.de>
2022-12-27 00:58:14 +0100

    filter table + additional style changes

--- src/lib/components/MatchTable.svelte
+++ src/lib/components/MatchTable.svelte
@@ -5,7 +5,9 @@
 	export let matches: Match[];
 
 	let selectedMana: Mana[] = [];
-	$: tableBody = filterTable(selectedMana);
+	let sortAsc: boolean = true;
+	let currSort: number = 0;
+	$: tableBody = filterTableForMana(selectedMana);
 
 	const renderColumn = (column: string, index: number) => {
 		if (index === Column.COLOR) {
@@ -23,25 +25,25 @@
 		if (index === Column.WINRATE) {
 			const percentageNumber = Number(column);
 			if (percentageNumber > 50) {
-				return `<span class="text-green-300">${column}%</span>`;
+				return `<span class="text-green-200">${column}%</span>`;
 			}
 
 			if (percentageNumber === 50) {
-				return `<span class="text-yellow-300">${column}%</span>`;
+				return `<span class="text-yellow-100">${column}%</span>`;
 			}
 
 			if (percentageNumber < 50) {
-				return `<span class="text-red-500">${column}%</span>`;
+				return `<span class="text-red-300">${column}%</span>`;
 			}
 		}
-		return column;
+		return `<span class="text-white">${column}</span>`;
 	};
 
 	const changeMana = (event: CustomEvent<Mana[]>) => {
 		selectedMana = event.detail;
 	};
 
-	const filterTable = (selectedMana: Mana[]) => {
+	const filterTableForMana = (selectedMana: Mana[]) => {
 		console.log(selectedMana);
 		const res = matches
 			.slice(1)
@@ -49,6 +51,26 @@
 		console.log(res);
 
 		return res;
+	};
+
+	const handleClickHeader = (header: string, index: number) => {
+		if (index === 0) {
+			return;
+		}
+		const res = matches;
+		let filteredContent;
+		if (!sortAsc) {
+			filteredContent = res
+				.slice(1)
+				.sort((matchA: any, matchB: any) => matchA[index] - matchB[index]);
+		} else {
+			filteredContent = res
+				.slice(1)
+				.sort((matchA: any, matchB: any) => matchB[index] - matchA[index]);
+		}
+		sortAsc = !sortAsc;
+		currSort = index;
+		tableBody = filteredContent;
 	};
 </script>
 
@@ -59,15 +81,37 @@
 
 	<table class="table-fixed">
 		<thead class="border-b">
-			{#each matches[0] as row}
-				<th class="font-medium text-gray-100 px-4 py-2 text-left"> {row} </th>
+			{#each matches[0] as header, i}
+				<th
+					class="font-medium text-gray-100 px-4 py-2"
+					on:click={() => handleClickHeader(header, i)}
+				>
+					{header}
+					<svg
+						class="inline"
+						fill={`${currSort === i && currSort > 0 ? '#ffffff' : '#64748B'}`}
+						width="0.75rem"
+						height="0.75rem"
+						viewBox="0 0 16 16"
+						xmlns="http://www.w3.org/2000/svg"
+						><g
+							><polygon
+								points={`${
+									sortAsc
+										? '11.51 10.49 8 6.99 4.5 10.49 3.5 9.51 8 5.01 12.49 9.51 11.51 10.49'
+										: '8 10.99 3.5 6.5 4.5 5.5 8 9.01 11.51 5.5 12.49 6.5 8 10.99'
+								}`}
+							/></g
+						></svg
+					>
+				</th>
 			{/each}
 		</thead>
 		<tbody>
 			{#each tableBody as row}
 				<tr class="border-b">
 					{#each row as col, i}
-						<td class="font-medium px-4 py-2 whitespace-nowrap">
+						<td class="font-medium px-4 py-2 whitespace-nowrap text-center">
 							{@html renderColumn(col, i)}
 						</td>
 					{/each}
