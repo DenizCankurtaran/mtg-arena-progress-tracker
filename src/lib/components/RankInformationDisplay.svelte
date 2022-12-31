3f0372c44423c8394dc0b7952f4e5c52a3e77e8a
Görkem Yalcinkaya <goerkem.yalcinkaya@gmx.de>
2022-12-31 16:17:12 +0100

    better display of out of table stats

--- src/lib/components/RankInformationDisplay.svelte
+++ src/lib/components/RankInformationDisplay.svelte
@@ -2,12 +2,18 @@
 	import getRank from '$lib/util/getRank';
 	import type { Rank } from '$lib/util/types';
 
+	import moment from 'moment';
+
 	export let rank: Rank;
+
+	const today = moment().startOf('day');
+	const lastMatch = moment(new Date(rank[0])).startOf('day');
 </script>
 
-<div class="font-small text-gray-100 ">
-	<div>{new Date(rank[0]).toDateString()}</div>
-	<div>{getRank(rank)}</div>
-	<div>Games {rank[5]}</div>
-	<div>Winrate {rank[6]}%</div>
+<div class="font-small text-gray-100 my-auto">
+	<div title={`Wins: ${rank[3]} \nLoses: ${rank[4]}`}>Games: {rank[5]}</div>
+	<div>Winrate: {Math.round(parseInt(rank[6]))}%</div>
+	<div>
+		last Match: {today.isSame(lastMatch) ? 'Today' : lastMatch.format('DD.MM.YYYY')}
+	</div>
 </div>
