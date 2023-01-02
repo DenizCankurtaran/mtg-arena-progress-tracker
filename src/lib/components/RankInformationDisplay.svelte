b41c20fb875914f504bd9ad051a47b049612681b
Deniz Cankurtaran <deniz.cankurtaran@hotmail.de>
2023-01-02 11:04:32 +0100

    remove moment.js

--- src/lib/components/RankInformationDisplay.svelte
+++ src/lib/components/RankInformationDisplay.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
-	import getRank from '$lib/util/getRank';
 	import type { Rank } from '$lib/util/types';
-
-	import moment from 'moment';
 
 	export let rank: Rank;
 
-	const today = moment().startOf('day');
-	const lastMatch = moment(new Date(rank[0])).startOf('day');
+	const isPast = () => {
+    	const lastRowDate = new Date(rank[0]);
+		const oneDay = 1000 * (60 * 60) * 24;
+    	return (new Date().getTime() - lastRowDate.getTime() > oneDay)
+	}
+
 </script>
 
 <div class="font-small text-gray-100 my-auto">
 	<div title={`Wins: ${rank[3]} \nLoses: ${rank[4]}`}>Games: {rank[5]}</div>
 	<div>Winrate: {Math.round(parseInt(rank[6]))}%</div>
 	<div>
-		last Match: {today.isSame(lastMatch) ? 'Today' : lastMatch.format('DD.MM.YYYY')}
+		last Match: { isPast() ? new Date(rank[0]).toDateString(): 'Today' }
 	</div>
 </div>
